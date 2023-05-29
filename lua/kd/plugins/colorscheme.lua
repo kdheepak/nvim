eed6ca51b62f15b4da5fbc90d6e36977e873b3f0
Dheepak Krishnamurthy <me@kdheepak.com>
2023-05-28 22:00:00 -0400

    chore: Update files ♻️

--- lua/kd/plugins/colorscheme.lua
+++ lua/kd/plugins/colorscheme.lua
@@ -7,7 +7,15 @@ return {
       local catppuccin = require("catppuccin")
       catppuccin.setup({})
       vim.g.catppuccin_flavour = "mocha" -- latte, frappe, macchiato, mocha
-      vim.cmd([[colorscheme catppuccin]])
     end
+  },
+  {
+    'projekt0n/github-nvim-theme',
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require('github-theme').setup({})
+      vim.cmd('colorscheme github_light')
+    end,
   }
 }
