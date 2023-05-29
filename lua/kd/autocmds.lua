acc687f0f1060a3847537a9aebf67f157331574c
Dheepak Krishnamurthy <me@kdheepak.com>
2023-05-28 23:57:12 -0400

    chore: Update files ♻️

--- lua/kd/autocmds.lua
+++ lua/kd/autocmds.lua
@@ -95,4 +95,15 @@ augroup("KDAutocmds", function()
     end,
     { pattern = "BDeletePost*" }
   )
+
+  -- LuaSnip Snippet History Fix
+  autocmd("ModeChanged", function()
+      if ((vim.v.event.old_mode == 's' and vim.v.event.new_mode == 'n') or vim.v.event.old_mode == 'i')
+          and require('luasnip').session.current_nodes[vim.api.nvim_get_current_buf()]
+          and not require('luasnip').session.jump_active
+      then
+          require('luasnip').unlink_current()
+      end
+    end
+  )
 end)
--- lua/kd/lazy.lua
+++ lua/kd/lazy.lua
@@ -23,7 +23,7 @@ require("lazy").setup({
   change_detection = {
     -- automatically check for config file changes and reload the ui
     enabled = false,
-    notify = true, -- get a notification when changes are found
+    notify = false, -- get a notification when changes are found
   },
 })
 
