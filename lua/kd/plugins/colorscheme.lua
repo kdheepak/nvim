d0095b9918ff4859a61e208eb74e79fcd3ac32fc
Dheepak Krishnamurthy <me@kdheepak.com>
2023-05-29 23:21:37 -0400

    chore: Update files ♻️

--- lua/kd/plugins/colorscheme.lua
+++ lua/kd/plugins/colorscheme.lua
@@ -16,7 +16,7 @@ return {
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       require("github-theme").setup({})
-      vim.cmd("colorscheme github_light")
+      vim.cmd("colorscheme github_light_colorblind")
     end,
   },
 }
