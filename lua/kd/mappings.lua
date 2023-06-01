9253b26c9ab0ab77a2ae4be4284b863b38a4034b
Dheepak Krishnamurthy <me@kdheepak.com>
2023-06-01 01:34:01 -0400

    chore: Update files ♻️

--- lua/kd/mappings.lua
+++ lua/kd/mappings.lua
@@ -336,6 +336,22 @@ end, {
 nnoremap("<leader>fg", function(opts)
   opts = opts or {}
   opts.continue_last_search = true
+  local function is_git_repo()
+    vim.fn.system("git rev-parse --is-inside-work-tree")
+
+    return vim.v.shell_error == 0
+  end
+
+  local function get_git_root()
+    local dot_git_path = vim.fn.finddir(".git", ".;")
+    return vim.fn.fnamemodify(dot_git_path, ":h")
+  end
+
+  if is_git_repo() then
+    opts = {
+      cwd = get_git_root(),
+    }
+  end
   require("telescope.builtin").live_grep(opts)
 end, {
   desc = "Live Grep",
--- lua/kd/plugins/fuzzy.lua
+++ lua/kd/plugins/fuzzy.lua
@@ -19,6 +19,66 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local actions = require("telescope.actions")
+      local telescopeConfig = require("telescope.config")
+
+      -- Clone the default Telescope configuration
+      local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+
+      -- I want to search in hidden/dot files.
+      table.insert(vimgrep_arguments, "--hidden")
+      -- I don't want to search in the `.git` directory.
+      table.insert(vimgrep_arguments, "--glob")
+      table.insert(vimgrep_arguments, "!**/.git/*")
+
+      require("telescope").setup({
+        defaults = {
+          selection_caret = " ",
+          vimgrep_arguments = vimgrep_arguments,
+          initial_mode = "insert",
+          selection_strategy = "reset",
+          sorting_strategy = "ascending",
+          layout_strategy = "horizontal",
+          layout_config = {
+            prompt_position = "top",
+            vertical = { mirror = false },
+          },
+          mappings = {
+            i = {
+              ["<esc>"] = actions.close,
+              ["<c-d>"] = actions.delete_buffer + actions.move_to_top,
+
+              ["<CR>"] = actions.select_default,
+              ["<C-s>"] = actions.select_horizontal,
+              ["<C-v>"] = actions.select_vertical,
+
+              ["<C-t>"] = actions.select_tab,
+
+              ["<C-l>"] = actions.complete_tag,
+              ["<C-h>"] = actions.which_key, -- keys from pressing <C-h>
+
+              ["<C-n>"] = actions.cycle_history_next,
+              ["<C-p>"] = actions.cycle_history_prev,
+
+              ["<C-b>"] = actions.results_scrolling_up,
+              ["<C-f>"] = actions.results_scrolling_down,
+
+              ["<C-c>"] = actions.close,
+
+              ["<C-j>"] = actions.move_selection_next,
+              ["<C-k>"] = actions.move_selection_previous,
+            },
+          },
+        },
+        pickers = {
+          find_files = {
+            -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+            find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+          },
+        },
+      })
+    end,
   },
   {
     "nvim-telescope/telescope-fzf-native.nvim",
