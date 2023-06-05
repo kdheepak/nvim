2db690c06f418e28227dc427a03def65dfbba250
Dheepak Krishnamurthy <me@kdheepak.com>
2023-06-05 06:20:25 -0400

    chore: Update files ♻️

--- lua/kd/plugins/colorscheme.lua
+++ lua/kd/plugins/colorscheme.lua
@@ -4,9 +4,64 @@ return {
     name = "catppuccin",
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
+    dependencies = {
+      {
+        "lukas-reineke/indent-blankline.nvim",
+        opts = {
+          buftype_exclude = {
+            "nofile",
+            "terminal",
+          },
+          filetype_exclude = {
+            "help",
+            "startify",
+            "aerial",
+            "alpha",
+            "dashboard",
+            "lazy",
+            "neogitstatus",
+            "NvimTree",
+            "neo-tree",
+            "Trouble",
+          },
+          context_patterns = {
+            "class",
+            "return",
+            "function",
+            "method",
+            "^if",
+            "^while",
+            "jsx_element",
+            "^for",
+            "^object",
+            "^table",
+            "block",
+            "arguments",
+            "if_statement",
+            "else_clause",
+            "jsx_element",
+            "jsx_self_closing_element",
+            "try_statement",
+            "catch_clause",
+            "import_statement",
+            "operation_type",
+          },
+          show_trailing_blankline_indent = true,
+          use_treesitter = true,
+          show_current_context = false,
+        },
+      },
+    },
     config = function()
       local catppuccin = require("catppuccin")
-      catppuccin.setup({})
+      catppuccin.setup({
+        integrations = {
+          indent_blankline = {
+            enabled = true,
+            colored_indent_levels = true,
+          },
+        },
+      })
       vim.g.catppuccin_flavour = "mocha" -- latte, frappe, macchiato, mocha
       vim.cmd("colorscheme catppuccin")
     end,
