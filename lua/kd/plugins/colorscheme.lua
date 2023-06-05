cb4c35269f9d5b470cf11a57a05b0d03ba3f8d0b
Dheepak Krishnamurthy <me@kdheepak.com>
2023-06-05 06:24:42 -0400

    chore: Update files ♻️

--- lua/kd/plugins/colorscheme.lua
+++ lua/kd/plugins/colorscheme.lua
@@ -6,6 +6,33 @@ return {
     priority = 1000, -- make sure to load this before all the other start plugins
     dependencies = {
       {
+        "echasnovski/mini.indentscope",
+        version = "*", -- wait till new 0.7.0 release to put it back on semver
+        event = "BufReadPre",
+        opts = {
+          -- symbol = "▏",
+          symbol = "│",
+          options = { try_as_border = true },
+        },
+        config = function(_, opts)
+          vim.api.nvim_create_autocmd("FileType", {
+            pattern = {
+              "help",
+              "alpha",
+              "dashboard",
+              "neo-tree",
+              "Trouble",
+              "lazy",
+              "mason",
+            },
+            callback = function()
+              vim.b.miniindentscope_disable = true
+            end,
+          })
+          require("mini.indentscope").setup(opts)
+        end,
+      },
+      {
         "lukas-reineke/indent-blankline.nvim",
         opts = {
           buftype_exclude = {
