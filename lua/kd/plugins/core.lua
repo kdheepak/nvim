ef96ffa75b335193015039a2f14630611db1eed6
Dheepak Krishnamurthy <me@kdheepak.com>
2023-06-15 22:56:34 -0400

    feat: Update files ✨

--- lua/kd/plugins/core.lua
+++ lua/kd/plugins/core.lua
@@ -66,6 +66,41 @@ return {
         -- This field is only relevant if direction is set to 'float'
         float_opts = { border = "curved" },
       })
+      local Terminal = require("toggleterm.terminal").Terminal
+
+      local lazygit = Terminal:new({
+        cmd = "lazygit",
+        hidden = true,
+        direction = "float",
+        on_open = function(term)
+          vim.cmd("startinsert!")
+          vim.api.nvim_buf_set_keymap(0, "t", "<esc>", "<cmd>close<CR>", { silent = false, noremap = true })
+          if vim.fn.mapcheck("<esc>", "t") ~= "" then
+            vim.api.nvim_buf_del_keymap(term.bufnr, "t", "<esc>")
+          end
+        end,
+      })
+      local function lazygit_toggle()
+        lazygit:toggle()
+      end
+
+      local lazygit_log = Terminal:new({
+        cmd = "lazygit log",
+        hidden = true,
+        direction = "float",
+        on_open = function(term)
+          vim.cmd("startinsert!")
+          vim.api.nvim_buf_set_keymap(0, "t", "<esc>", "<cmd>close<CR>", { silent = false, noremap = true })
+          if vim.fn.mapcheck("<esc>", "t") ~= "" then
+            vim.api.nvim_buf_del_keymap(term.bufnr, "t", "<esc>")
+          end
+        end,
+      })
+      local function lazygit_log_toggle()
+        lazygit_log:toggle()
+      end
+      nnoremap("<leader>gg", lazygit_toggle, { desc = "LazyGit Toggle" })
+      nnoremap("<leader>gl", lazygit_log_toggle, { desc = "LazyGit Log" })
     end,
     event = "VeryLazy",
   },
