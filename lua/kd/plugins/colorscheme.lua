045850797c0e126087363f60c96d469920537967
Dheepak Krishnamurthy <me@kdheepak.com>
2023-07-03 01:46:19 -0400

    chore: Update files ♻️

--- lua/kd/plugins/colorscheme.lua
+++ lua/kd/plugins/colorscheme.lua
@@ -6,18 +6,12 @@ return {
     priority = 1000, -- make sure to load this before all the other start plugins
     dependencies = {
       {
-        "Yggdroot/indentLine",
-        config = function()
-          vim.cmd("let g:indentLine_char_list = ['│']")
-        end,
-      },
-      {
         "echasnovski/mini.indentscope",
         version = "*", -- wait till new 0.7.0 release to put it back on semver
         event = "BufReadPre",
         opts = {
-          -- symbol = "▏",
-          symbol = "│",
+          symbol = "▏",
+          -- symbol = "│",
           options = { try_as_border = true },
         },
         config = function(_, opts)
@@ -38,55 +32,6 @@ return {
           require("mini.indentscope").setup(opts)
         end,
       },
-      -- {
-      --   "lukas-reineke/indent-blankline.nvim",
-      --   opts = {
-      --     buftype_exclude = {
-      --       "nofile",
-      --       "terminal",
-      --     },
-      --     filetype_exclude = {
-      --       "help",
-      --       "startify",
-      --       "aerial",
-      --       "alpha",
-      --       "dashboard",
-      --       "lazy",
-      --       "neogitstatus",
-      --       "NvimTree",
-      --       "neo-tree",
-      --       "Trouble",
-      --     },
-      --     context_patterns = {
-      --       "class",
-      --       "return",
-      --       "function",
-      --       "method",
-      --       "^if",
-      --       "^while",
-      --       "jsx_element",
-      --       "^for",
-      --       "^object",
-      --       "^table",
-      --       "block",
-      --       "arguments",
-      --       "if_statement",
-      --       "else_clause",
-      --       "jsx_element",
-      --       "jsx_self_closing_element",
-      --       "try_statement",
-      --       "catch_clause",
-      --       "import_statement",
-      --       "operation_type",
-      --     },
-      --     show_trailing_blankline_indent = false,
-      --     use_treesitter = false,
-      --     char = "▏",
-      --     context_char = "▏",
-      --     show_current_context = false,
-      --     space_char_blankline = " ",
-      --   },
-      -- },
     },
     config = function()
       vim.opt.list = true
