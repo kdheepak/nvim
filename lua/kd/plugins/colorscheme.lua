c23df2af181e2dcc055610bdc821398b071dded1
Dheepak Krishnamurthy <me@kdheepak.com>
2024-01-07 20:35:42 -0500

    chore: Update files ♻️

--- lua/kd/plugins/colorscheme.lua
+++ lua/kd/plugins/colorscheme.lua
@@ -1,5 +1,38 @@
 return {
   {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  {
+    "nyoom-engineering/oxocarbon.nvim",
+    -- Add in any other configuration;
+    --   event = foo,
+    --   config = bar
+    --   end,
+  },
+  {
+    "bluz71/vim-nightfly-colors",
+    name = "nightfly",
+    lazy = false,
+    priority = 1000,
+  },
+  {
+    "bluz71/vim-moonfly-colors",
+    name = "moonfly",
+    lazy = false,
+    priority = 1000,
+    -- config = function()
+    --   vim.opt.list = true
+    --   vim.opt.listchars:append("space:⋅")
+    --   vim.cmd([[colorscheme moonfly]])
+    -- end,
+  },
+  {
+    "rebelot/kanagawa.nvim",
+  },
+  {
     "catppuccin/nvim",
     name = "catppuccin",
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
@@ -33,22 +66,22 @@ return {
         end,
       },
     },
+    -- config = function()
+    --   vim.opt.list = true
+    --   vim.opt.listchars:append("space:⋅")
+    --   local catppuccin = require("catppuccin")
+    --   catppuccin.setup({})
+    --   vim.g.catppuccin_flavour = "mocha" -- latte, frappe, macchiato, mocha
+    --   vim.cmd("colorscheme catppuccin")
+    -- end,
+  },
+  {
+    "projekt0n/github-nvim-theme",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      vim.opt.list = true
-      vim.opt.listchars:append("space:⋅")
-      local catppuccin = require("catppuccin")
-      catppuccin.setup({})
-      vim.g.catppuccin_flavour = "mocha" -- latte, frappe, macchiato, mocha
-      vim.cmd("colorscheme catppuccin")
+      require("github-theme").setup({})
+      vim.cmd("colorscheme github_light")
     end,
   },
-  -- {
-  --   "projekt0n/github-nvim-theme",
-  --   lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  --   priority = 1000, -- make sure to load this before all the other start plugins
-  --   config = function()
-  --     -- require("github-theme").setup({})
-  --     -- vim.cmd("colorscheme github_light_colorblind")
-  --   end,
-  -- },
 }
