e8d59d013c8bc783b774070e088a3c856cb94b18
Dheepak Krishnamurthy <me@kdheepak.com>
2024-02-23 17:21:58 -0500

    chore: Update files

--- lua/kd/plugins/core.lua
+++ lua/kd/plugins/core.lua
@@ -49,6 +49,99 @@ return {
       require("detect-language").setup({})
     end,
   },
+  {
+    "akinsho/toggleterm.nvim",
+    config = function()
+      local nnoremap = require("kd.utils").nnoremap
+      local tnoremap = require("kd.utils").tnoremap
+      local toggleterm = require("toggleterm")
+      local terminal = require("toggleterm.terminal")
+
+      local function open_new_terminal(direction)
+        if #terminal.get_all() ~= 0 then
+          if direction == "horizontal" then
+            direction = "vertical"
+          else
+            direction = "horizontal"
+          end
+        end
+        local term = terminal.Terminal:new({ id = #terminal.get_all() + 1, direction = direction, size = 0.5 })
+        term:toggle()
+      end
+
+      local function toggle_or_open_new_terminal(direction)
+        open_new_terminal(direction)
+      end
+
+      nnoremap("<leader>/", function()
+        toggle_or_open_new_terminal("horizontal")
+      end, { desc = "Split terminal horizontally" })
+      nnoremap("<leader>\\", function()
+        toggle_or_open_new_terminal("vertical")
+      end, { desc = "Split terminal vertically" })
+
+      nnoremap("<Leader>tt", "<cmd>ToggleTerm<CR>", { desc = "Toggle terminal" })
+
+      tnoremap("<ESC><ESC>", "<C-\\><C-n>", { desc = "ESC to normal mode" })
+      tnoremap("<S-Space>", "<Space>")
+
+      require("toggleterm").setup({
+        -- size can be a number or function which is passed the current terminal
+        size = function(term)
+          if term.direction == "horizontal" then
+            return vim.o.lines * 0.5
+          elseif term.direction == "vertical" then
+            return vim.o.columns * 0.5
+          end
+        end,
+        -- size can be a number or function which is passed the current terminal
+        hide_numbers = true, -- hide the number column in toggleterm buffers
+        shade_terminals = false,
+        start_in_insert = true,
+        insert_mappings = false,
+        persist_size = false,
+        direction = "float",
+        close_on_exit = true,
+        -- This field is only relevant if direction is set to 'float'
+        float_opts = { border = "curved" },
+      })
+      local Terminal = require("toggleterm.terminal").Terminal
+
+      local lazygit = Terminal:new({
+        cmd = "lazygit",
+        hidden = true,
+        direction = "float",
+        on_open = function(term)
+          vim.cmd("startinsert!")
+          vim.api.nvim_buf_set_keymap(0, "t", "<esc>", "<cmd>close<CR>", { silent = false, noremap = true })
+          if vim.fn.mapcheck("<esc>", "t") ~= "" then
+            vim.api.nvim_buf_del_keymap(term.bufnr, "t", "<esc>")
+          end
+        end,
+      })
+      local function lazygit_toggle()
+        lazygit:toggle()
+      end
+
+      local lazygit_log = Terminal:new({
+        cmd = "lazygit log",
+        hidden = true,
+        direction = "float",
+        on_open = function(term)
+          vim.cmd("startinsert!")
+          vim.api.nvim_buf_set_keymap(0, "t", "<esc>", "<cmd>close<CR>", { silent = false, noremap = true })
+          if vim.fn.mapcheck("<esc>", "t") ~= "" then
+            vim.api.nvim_buf_del_keymap(term.bufnr, "t", "<esc>")
+          end
+        end,
+      })
+      local function lazygit_log_toggle()
+        lazygit_log:toggle()
+      end
+      nnoremap("<leader>gg", lazygit_toggle, { desc = "LazyGit Toggle" })
+      nnoremap("<leader>gl", lazygit_log_toggle, { desc = "LazyGit Log" })
+    end,
+  },
   { "GutenYe/json5.vim", ft = "json" },
   { "GCBallesteros/jupytext.vim", ft = { "ipynb", "python", "markdown" } },
   {
