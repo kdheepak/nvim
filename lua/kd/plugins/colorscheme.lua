5db9fcb009ed4ada2be11168ced83388177c4c37
Dheepak Krishnamurthy <me@kdheepak.com>
2024-06-13 22:37:15 -0400

    chore: Update files

--- lua/kd/plugins/colorscheme.lua
+++ lua/kd/plugins/colorscheme.lua
@@ -13,4 +13,17 @@ return {
       vim.cmd.colorscheme("rose-pine")
     end,
   },
+  -- Or with configuration
+  {
+    "projekt0n/github-nvim-theme",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    -- config = function()
+    --   require("github-theme").setup({
+    --     -- ...
+    --   })
+    --
+    --   vim.cmd("colorscheme github_light_colorblind")
+    -- end,
+  },
 }
