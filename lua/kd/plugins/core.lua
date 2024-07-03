27a4346bdb112f2cb69283d65cce97ff0e8f72e4
Dheepak Krishnamurthy <me@kdheepak.com>
2024-07-03 14:25:41 -0400

    chore: Update files

--- lua/kd/plugins/core.lua
+++ lua/kd/plugins/core.lua
@@ -17,18 +17,45 @@ return {
   {
     "echasnovski/mini.nvim",
     version = false,
-    dependencies = { "nvim-tree/nvim-web-devicons" },
+    dependencies = { "nvim-tree/nvim-web-devicons", "nvim-treesitter/nvim-treesitter-textobjects" },
     config = function()
       require("mini.ai").setup()
       require("mini.operators").setup()
       require("mini.indentscope").setup()
       require("mini.align").setup()
       require("mini.bracketed").setup()
+      require("mini.bufremove").setup({
+        keys = {
+          {
+            "<leader>bd",
+            function()
+              require("mini.bufremove").delete(0, false)
+            end,
+            desc = "Delete Buffer",
+          },
+          {
+            "<leader>bD",
+            function()
+              require("mini.bufremove").delete(0, true)
+            end,
+            desc = "Delete Buffer (Force)",
+          },
+        },
+      })
+      require("mini.comment").setup({
+        options = {
+          custom_commentstring = function()
+            return require("ts_context_commentstring.internal").calculate_commentstring() or vim.bo.commentstring
+          end,
+        },
+      })
+      require("mini.pairs").setup()
+      require("mini.splitjoin").setup()
+      require("mini.surround").setup()
     end,
   },
   { "mbbill/undotree", event = "VeryLazy" },
   { "tpope/vim-abolish", event = "VeryLazy" }, -- convert camel to snake
-  { "tpope/vim-surround", event = "VeryLazy" }, -- all about surroundings: parentheses, brackets, quotes, XML tags, and more.
   { "tpope/vim-repeat", event = "VeryLazy" }, -- repeat.vim remaps . in a way that plugins can tap into it.
   { "tpope/vim-speeddating", event = "VeryLazy" }, -- Tools for working with dates
   { "tpope/vim-eunuch", event = "VeryLazy" }, -- vim sugar for UNIX shell commands like :Rename
--- lua/kd/plugins/editor.lua
+++ lua/kd/plugins/editor.lua
@@ -209,16 +209,6 @@ return {
     },
   },
 
-  -- buffer remove
-  {
-    "echasnovski/mini.bufremove",
-    -- stylua: ignore
-    keys = {
-      { "<leader>bd", function() require("mini.bufremove").delete(0, false) end, desc = "Delete Buffer" },
-      { "<leader>bD", function() require("mini.bufremove").delete(0, true) end, desc = "Delete Buffer (Force)" },
-    },
-  },
-
   -- better diagnostics list and others
   {
     "folke/trouble.nvim",
@@ -262,8 +252,6 @@ return {
     config = true,
     -- stylua: ignore
     keys = {
-      { "]t", function() require("todo-comments").jump_next() end, desc = "Next todo comment" },
-      { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous todo comment" },
       { "<leader>xt", "<cmd>TodoTrouble<cr>", desc = "Todo (Trouble)" },
       { "<leader>xT", "<cmd>TodoTrouble keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme (Trouble)" },
     },
@@ -274,40 +262,4 @@ return {
 
   -- comments
   { "JoosepAlviste/nvim-ts-context-commentstring", lazy = true },
-  {
-    "echasnovski/mini.comment",
-    event = "VeryLazy",
-    opts = {
-      options = {
-        custom_commentstring = function()
-          return require("ts_context_commentstring.internal").calculate_commentstring() or vim.bo.commentstring
-        end,
-      },
-    },
-  },
-
-  -- better text-objects
-  {
-    "echasnovski/mini.ai",
-    -- keys = {
-    --   { "a", mode = { "x", "o" } },
-    --   { "i", mode = { "x", "o" } },
-    -- },
-    event = "VeryLazy",
-    dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
-    opts = function()
-      local ai = require("mini.ai")
-      return {
-        n_lines = 500,
-        custom_textobjects = {
-          o = ai.gen_spec.treesitter({
-            a = { "@block.outer", "@conditional.outer", "@loop.outer" },
-            i = { "@block.inner", "@conditional.inner", "@loop.inner" },
-          }, {}),
-          f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
-          c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }, {}),
-        },
-      }
-    end,
-  },
 }
