99a0a1249e6eebe87760acbbe49a2f64281803fe
Dheepak Krishnamurthy <me@kdheepak.com>
2024-07-04 18:39:42 -0400

    chore: Update files

--- lua/kd/plugins/git.lua
+++ lua/kd/plugins/git.lua
@@ -19,6 +19,25 @@ return {
     config = true,
   },
   {
+    "kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
+  },
+  {
     "akinsho/git-conflict.nvim",
     version = "*",
     config = true,
