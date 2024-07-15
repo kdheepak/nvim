b86957485cb57773e80cd6a996a6a30f5a9407ae
Dheepak Krishnamurthy <me@kdheepak.com>
2024-07-15 02:21:50 -0400

    chore: Update files

--- lua/kd/plugins/lualine.lua
+++ lua/kd/plugins/lualine.lua
@@ -7,21 +7,7 @@ return {
     require("lualine").setup({
       options = {
         icons_enabled = true,
-        theme = "rose-pine",
-        component_separators = { left = "", right = "" },
-        section_separators = { left = "", right = "" },
-        disabled_filetypes = {
-          statusline = {},
-          winbar = {},
-        },
-        ignore_focus = {},
-        always_divide_middle = true,
-        globalstatus = false,
-        refresh = {
-          statusline = 1000,
-          tabline = 1000,
-          winbar = 1000,
-        },
+        theme = "rosepine",
       },
       sections = {
         lualine_a = { "mode" },
