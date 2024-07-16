738a3939f28f1a779974dc9a0e684a5506eb3daa
Dheepak Krishnamurthy <me@kdheepak.com>
2024-07-16 02:35:15 -0400

    chore: Update files

--- lua/kd/plugins/lualine.lua
+++ lua/kd/plugins/lualine.lua
@@ -7,7 +7,7 @@ return {
     require("lualine").setup({
       options = {
         icons_enabled = true,
-        theme = "rosepine",
+        theme = "rose-pine",
       },
       sections = {
         lualine_a = { "mode" },
