7b0385f3145653d8069ff366f190dd9a81940f18
Dheepak Krishnamurthy <noreply@kdheepak.com>
2024-09-03 08:04:39 -0400

    chore: Update files

--- lua/kd/plugins/highstr.lua
+++ lua/kd/plugins/highstr.lua
@@ -2,7 +2,6 @@ return {
   "Pocco81/HighStr.nvim",
   config = function()
     local vnoremap = require("kd.utils").vnoremap
-    local nnoremap = require("kd.utils").nnoremap
     local wk = require("which-key")
     wk.add({ { "<leader>h", group = "+highlight" } })
     vnoremap("<leader>h1", ":<c-u>HSHighlight 1<CR>", { desc = "Highlight 1" })
@@ -15,8 +14,8 @@ return {
     vnoremap("<leader>h8", ":<c-u>HSHighlight 8<CR>", { desc = "Highlight 8" })
     vnoremap("<leader>h9", ":<c-u>HSHighlight 9<CR>", { desc = "Highlight 9" })
     vnoremap("<leader>h0", ":<c-u>HSRmHighlight<CR>", { desc = "Highlight Remove" })
-    vnoremap("<leader>hh", ":<c-u>HSRmHighlight rm_all<CR>", { desc = "Highlight Remove all" })
-    nnoremap("<leader>hh", ":<c-u>HSRmHighlight rm_all<CR>", { desc = "Highlight Remove all" })
+    -- vnoremap("<leader>hh", ":<c-u>HSRmHighlight rm_all<CR>", { desc = "Highlight Remove all" })
+    -- nnoremap("<leader>hh", ":<c-u>HSRmHighlight rm_all<CR>", { desc = "Highlight Remove all" })
   end,
   event = "VeryLazy",
 }
