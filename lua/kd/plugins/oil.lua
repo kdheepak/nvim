1c534c83a3314ce682433fc070d917426f46fbd6
Dheepak Krishnamurthy <noreply@kdheepak.com>
2024-09-08 10:45:17 -0400

    chore: Update files

--- lua/kd/plugins/oil.lua
+++ lua/kd/plugins/oil.lua
@@ -1,6 +1,11 @@
 return {
   "stevearc/oil.nvim",
-  opts = {},
-  -- Optional dependencies
   dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("oil").setup({
+      win_options = {
+        signcolumn = "yes:2",
+      },
+    })
+  end,
 }
