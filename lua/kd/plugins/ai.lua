3939439a2eea0e4f8c67064303429fc8d31f75ab
Dheepak Krishnamurthy <noreply@kdheepak.com>
2025-02-02 08:45:07 -0500

    chore: Update files

--- lua/kd/plugins/ai.lua
+++ lua/kd/plugins/ai.lua
@@ -4,41 +4,23 @@ return {
   build = "make",
   config = function()
     require("avante").setup({
-      provider = "openrouter",
+      provider = "claude_sonnet",
       vendors = {
-        openrouter = {
+        claude_sonnet = {
+          __inherited_from = "openai",
           api_key_name = "OPENROUTER_API_KEY",
-          endpoint = "https://openrouter.ai/api/v1/chat/completions",
-          -- model = "anthropic/claude-3.5-sonnet",
-          model = "deepseek/deepseek-chat",
-          parse_curl_args = function(opts, code_opts)
-            return {
-              url = opts.endpoint,
-              headers = {
-                ["Content-Type"] = "application/json",
-                ["Authorization"] = "Bearer " .. os.getenv(opts.api_key_name),
-              },
-              body = {
-                model = opts.model,
-                messages = require("avante.providers").openai.parse_messages(code_opts), -- you can make your own message, but this is very advanced
-                temperature = 0,
-                max_tokens = 4096,
-                stream = true,
-              },
-            }
-          end,
-          parse_response_data = function(data_stream, event_state, opts)
-            require("avante.providers").openai.parse_response(data_stream, event_state, opts)
-          end,
+          endpoint = "https://openrouter.ai/api/v1/",
+          model = "anthropic/claude-3.5-sonnet:beta",
+          temperature = 0,
+          max_tokens = 8192,
         },
       },
-      openai = {
-        endpoint = "https://api.openai.com/v1",
-        model = "gpt-3.5-turbo",
-        timeout = 30000, -- Timeout in milliseconds
-        temperature = 0,
-        max_tokens = 4096,
-        ["local"] = false,
+      behaviour = {
+        auto_suggestions = false, -- Experimental stage
+        auto_set_highlight_group = true,
+        auto_set_keymaps = true,
+        auto_apply_diff_after_generation = false,
+        support_paste_from_clipboard = false,
       },
     })
   end,
@@ -65,6 +47,30 @@ return {
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-    "MeanderingProgrammer/render-markdown.nvim",
+    {
+      -- support for image pasting
+      "HakonHarnes/img-clip.nvim",
+      event = "VeryLazy",
+      opts = {
+        -- recommended settings
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+          -- required for Windows users
+          use_absolute_path = true,
+        },
+      },
+    },
+    {
+      -- Make sure to set this up properly if you have lazy=true
+      "MeanderingProgrammer/render-markdown.nvim",
+      opts = {
+        file_types = { "markdown", "Avante" },
+      },
+      ft = { "markdown", "Avante" },
+    },
   },
 }
