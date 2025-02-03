9d3acee00e0bb944668987bce3b90073cc5b9e84
Dheepak Krishnamurthy <1813121+kdheepak@users.noreply.github.com>
2025-02-03 12:30:33 -0500

    chore: Update files

--- lua/kd/plugins/git.lua
+++ lua/kd/plugins/git.lua
@@ -6,10 +6,23 @@ return {
     "NeogitOrg/neogit",
     dependencies = {
       "nvim-lua/plenary.nvim", -- required
-      "sindrets/diffview.nvim", -- optional - Diff integration
-      "ibhagwan/fzf-lua", -- optional
+      "sindrets/diffview.nvim",
+      "ibhagwan/fzf-lua",
     },
-    config = true,
+    config = function()
+      require("neogit").setup({
+        disable_hint = true,
+        graph_style = "unicode",
+        kind = "split", -- opens neogit in a split
+        signs = {
+          -- { CLOSED, OPENED }
+          section = { "", "" },
+          item = { "", "" },
+          hunk = { "", "" },
+        },
+        integrations = { diffview = true }, -- adds integration with diffview.nvim
+      })
+    end,
   },
   {
     "kdheepak/lazygit.nvim",
@@ -24,12 +37,53 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim",
     },
-    -- setting the keybinding for LazyGit with 'keys' is recommended in
-    -- order to load the plugin when the command is run for the first time
     keys = {
       { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+      {
+        "<leader>gl",
+        "<cmd>LazyGitLog<cr>",
+        desc = "LazyGit log",
+      },
     },
   },
+  -- git signs
+  {
+    "lewis6991/gitsigns.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {
+      signs = {
+        add = { text = "▎" },
+        change = { text = "▎" },
+        delete = { text = "" },
+        topdelete = { text = "" },
+        changedelete = { text = "▎" },
+        untracked = { text = "▎" },
+      },
+      on_attach = function(buffer)
+        local function map(mode, l, r, desc)
+          require("kd.utils").map(mode, l, r, { buffer = buffer, desc = desc })
+        end
+        local gs = package.loaded.gitsigns
+        map("n", "]h", gs.next_hunk, "Next Hunk")
+        map("n", "[h", gs.prev_hunk, "Prev Hunk")
+        map({ "n", "v" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
+        map({ "n", "v" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
+        map("n", "<leader>ghS", gs.stage_buffer, "Stage Buffer")
+        map("n", "<leader>ghu", gs.undo_stage_hunk, "Undo Stage Hunk")
+        map("n", "<leader>ghR", gs.reset_buffer, "Reset Buffer")
+        map("n", "<leader>ghp", gs.preview_hunk, "Preview Hunk")
+        map("n", "<leader>ghb", function()
+          gs.blame_line({ full = true })
+        end, "Blame Line")
+        map("n", "<leader>ghd", gs.diffthis, "Diff This")
+        map("n", "<leader>ghD", function()
+          gs.diffthis("~")
+        end, "Diff This ~")
+        map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
+      end,
+    },
+  },
+
   {
     "akinsho/git-conflict.nvim",
     version = "*",
@@ -38,5 +92,40 @@ return {
   {
     "ruifm/gitlinker.nvim",
     dependencies = "nvim-lua/plenary.nvim",
+  },
+  {
+    "sindrets/diffview.nvim",
+    config = function()
+      require("diffview").setup({
+        view = {
+          -- Use 4-way diff (ours, base, theirs; local) for fixing conflicts
+          merge_tool = {
+            layout = "diff4_mixed",
+            disable_diagnostics = true,
+          },
+        },
+      })
+    end,
+  },
+  {
+    "isakbm/gitgraph.nvim",
+    opts = {
+      symbols = {
+        merge_commit = "M",
+        commit = "*",
+      },
+      format = {
+        timestamp = "%H:%M:%S %d-%m-%Y",
+        fields = { "hash", "timestamp", "author", "branch_name", "tag" },
+      },
+      hooks = {
+        on_select_commit = function(commit)
+          print("selected commit:", commit.hash)
+        end,
+        on_select_range_commit = function(from, to)
+          print("selected range:", from.hash, to.hash)
+        end,
+      },
+    },
   },
 }
