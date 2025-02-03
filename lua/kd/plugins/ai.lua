d50f3cb46c8fc98bb2d6dc8d270a576716fa1ccd
Dheepak Krishnamurthy <1813121+kdheepak@users.noreply.github.com>
2025-02-02 19:26:00 -0500

    chore: Update files

--- lua/kd/plugins/ai.lua
+++ lua/kd/plugins/ai.lua
@@ -1,76 +1,85 @@
 return {
-  "yetone/avante.nvim",
-  cmd = "AvanteAsk",
-  build = "make",
-  config = function()
-    require("avante").setup({
-      provider = "claude_sonnet",
-      vendors = {
-        claude_sonnet = {
-          __inherited_from = "openai",
-          api_key_name = "OPENROUTER_API_KEY",
-          endpoint = "https://openrouter.ai/api/v1/",
-          model = "anthropic/claude-3.5-sonnet:beta",
-          temperature = 0,
-          max_tokens = 8192,
-        },
-      },
-      behaviour = {
-        auto_suggestions = false, -- Experimental stage
-        auto_set_highlight_group = true,
-        auto_set_keymaps = true,
-        auto_apply_diff_after_generation = false,
-        support_paste_from_clipboard = false,
-      },
-    })
-  end,
-  keys = {
-    {
-      "<leader>aa",
-      function()
-        require("avante.api").ask()
-      end,
-      desc = "avante: ask",
-      mode = { "n", "v" },
-    },
-    {
-      "<leader>ae",
-      function()
-        require("avante.api").edit()
-      end,
-      desc = "avante: edit",
-      mode = "v",
-    },
+  {
+    "zbirenbaum/copilot.lua",
+    config = function()
+      require("copilot").setup({})
+    end,
   },
-  dependencies = {
-    "stevearc/dressing.nvim",
-    "nvim-lua/plenary.nvim",
-    "MunifTanjim/nui.nvim",
-    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-    {
-      -- support for image pasting
-      "HakonHarnes/img-clip.nvim",
-      event = "VeryLazy",
-      opts = {
-        -- recommended settings
-        default = {
-          embed_image_as_base64 = false,
-          prompt_for_file_name = false,
-          drag_and_drop = {
-            insert_mode = true,
+  {
+    "yetone/avante.nvim",
+    cmd = "AvanteAsk",
+    build = "make",
+    config = function()
+      require("avante").setup({
+        provider = "copilot",
+        vendors = {
+          claude_sonnet = {
+            __inherited_from = "openai",
+            api_key_name = "OPENROUTER_API_KEY",
+            endpoint = "https://openrouter.ai/api/v1/",
+            model = "anthropic/claude-3.5-sonnet:beta",
+            temperature = 0,
+            max_tokens = 8192,
           },
-          -- required for Windows users
-          use_absolute_path = true,
         },
+        behaviour = {
+          auto_suggestions = false, -- Experimental stage
+          auto_set_highlight_group = true,
+          auto_set_keymaps = true,
+          auto_apply_diff_after_generation = false,
+          support_paste_from_clipboard = false,
+        },
+      })
+    end,
+    keys = {
+      {
+        "<leader>aa",
+        function()
+          require("avante.api").ask()
+        end,
+        desc = "avante: ask",
+        mode = { "n", "v" },
+      },
+      {
+        "<leader>ae",
+        function()
+          require("avante.api").edit()
+        end,
+        desc = "avante: edit",
+        mode = "v",
       },
     },
-    {
-      -- Make sure to set this up properly if you have lazy=true
-      "MeanderingProgrammer/render-markdown.nvim",
-      opts = {
-        file_types = { "markdown", "Avante" },
+    dependencies = {
+      "zbirenbaum/copilot.lua", -- for providers='copilot'
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
       },
-      ft = { "markdown", "Avante" },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
     },
   },
 }
