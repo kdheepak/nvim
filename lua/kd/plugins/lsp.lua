e874217f5fb0e23ee4a83a1620c14993ac0136ca
Dheepak Krishnamurthy <noreply@kdheepak.com>
2025-02-07 19:46:54 -0500

    chore: Update files

--- lua/kd/plugins/lsp.lua
+++ lua/kd/plugins/lsp.lua
@@ -162,8 +162,7 @@ return {
           })
 
           local capabilities = vim.lsp.protocol.make_client_capabilities()
-          capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
-          capabilities.textDocument.completion.completionItem.snippetSupport = false
+          capabilities = require("blink.cmp").get_lsp_capabilities(capabilities)
 
           local lspconfig = require("lspconfig")
 
