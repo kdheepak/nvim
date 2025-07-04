803fae3c461a5d86eb875dd499c96ea40c271f74
Dheepak Krishnamurthy <1813121+kdheepak@users.noreply.github.com>
2025-07-04 03:10:26 -0400

    chore: Update files

--- lua/kd/plugins/codesnap.lua
+++ lua/kd/plugins/codesnap.lua
@@ -1,6 +1,9 @@
 return {
   "mistricky/codesnap.nvim",
   build = "make",
+  cond = function()
+    return jit.os ~= "Windows"
+  end,
   config = function()
     require("codesnap").setup({
       watermark = "",
