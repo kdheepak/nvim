a74e6956c49a76b47a71f4ba5c5aad182325e0a7
Dheepak Krishnamurthy <1813121+kdheepak@users.noreply.github.com>
2025-07-24 07:03:58 -0400

    feat(colorscheme): add rose-pine theme and update plugin configurations

--- lua/kd/plugins/colorscheme.lua
+++ lua/kd/plugins/colorscheme.lua
@@ -1,4 +1,11 @@
 return {
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    config = function()
+      vim.cmd("colorscheme rose-pine")
+    end,
+  },
   -- {
   --   "folke/tokyonight.nvim",
   --   lazy = false,
@@ -27,14 +34,12 @@ return {
   {
     "projekt0n/github-nvim-theme",
     name = "github-theme",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      require("github-theme").setup({
-        -- ...
-      })
-
-      vim.cmd("colorscheme github_light_tritanopia")
+      -- require("github-theme").setup({
+      --   -- ...
+      -- })
+      --
+      -- vim.cmd("colorscheme github_light_tritanopia")
     end,
   },
 }
