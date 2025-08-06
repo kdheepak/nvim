49850cc90fafe228078ef49076d1dcef2d085bb1
Dheepak Krishnamurthy <1813121+kdheepak@users.noreply.github.com>
2025-08-06 11:19:08 -0400

    feat(mappings): integrate smart-splits for window management and resizing

--- lua/kd/mappings.lua
+++ lua/kd/mappings.lua
@@ -14,19 +14,25 @@ vnoremap(">", ">gv")
 nnoremap(">", ">>_")
 nnoremap("<", ">>_")
 
--- Move to window using the <ctrl> hjkl keys
-map("n", "<C-h>", "<C-w>h", { desc = "Go to left window", remap = true })
-map("n", "<C-j>", "<C-w>j", { desc = "Go to lower window", remap = true })
-map("n", "<C-k>", "<C-w>k", { desc = "Go to upper window", remap = true })
-map("n", "<C-l>", "<C-w>l", { desc = "Go to right window", remap = true })
-
--- Move Lines
-map("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move down" })
-map("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move up" })
-map("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
-map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
-map("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
-map("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
+-- recommended mappings
+-- resizing splits
+-- these keymaps will also accept a range,
+-- for example `10<A-h>` will `resize_left` by `(10 * config.default_amount)`
+nnoremap("<A-h>", require("smart-splits").resize_left)
+nnoremap("<A-j>", require("smart-splits").resize_down)
+nnoremap("<A-k>", require("smart-splits").resize_up)
+nnoremap("<A-l>", require("smart-splits").resize_right)
+-- moving between splits
+nnoremap("<C-h>", require("smart-splits").move_cursor_left)
+nnoremap("<C-j>", require("smart-splits").move_cursor_down)
+nnoremap("<C-k>", require("smart-splits").move_cursor_up)
+nnoremap("<C-l>", require("smart-splits").move_cursor_right)
+nnoremap("<C-\\>", require("smart-splits").move_cursor_previous)
+-- swapping buffers between windows
+nnoremap("<leader><leader>h", require("smart-splits").swap_buf_left)
+nnoremap("<leader><leader>j", require("smart-splits").swap_buf_down)
+nnoremap("<leader><leader>k", require("smart-splits").swap_buf_up)
+nnoremap("<leader><leader>l", require("smart-splits").swap_buf_right)
 
 -- move to beginning of the line
 noremap("H", "^")
