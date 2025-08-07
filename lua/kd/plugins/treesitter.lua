825ea51554d92c9328baaae250eec9481b4589df
Dheepak Krishnamurthy <1813121+kdheepak@users.noreply.github.com>
2025-08-07 09:27:18 -0400

    feat(treesitter): enable textobjects and remove treesj plugin

--- lua/kd/plugins/treesitter.lua
+++ lua/kd/plugins/treesitter.lua
@@ -11,7 +11,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     dependencies = {
       "windwp/nvim-ts-autotag",
-      -- "nvim-treesitter/nvim-treesitter-textobjects",
+      "nvim-treesitter/nvim-treesitter-textobjects",
       "nvim-treesitter/playground",
     },
     cmd = {
