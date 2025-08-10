6cf491a98217f5755b35419ad53f1a16feba4cfe
Dheepak Krishnamurthy <1813121+kdheepak@users.noreply.github.com>
2025-08-10 07:03:51 -0400

    feat(plugins): add img-clip.nvim for clipboard image pasting

---
+++ lua/kd/plugins/img-clip.lua
@@ -0,0 +1,8 @@
+return {
+  "HakonHarnes/img-clip.nvim",
+  event = "VeryLazy",
+  opts = {},
+  keys = {
+    { "<leader>ip", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
+  },
+}
