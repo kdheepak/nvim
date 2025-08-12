fad437fb0ef0bbe6b10173d26652dd212b995c88
Dheepak Krishnamurthy <1813121+kdheepak@users.noreply.github.com>
2025-08-11 20:42:29 -0400

    feat(colorscheme): enable catppuccin with latte flavor

--- lua/kd/plugins/colorscheme.lua
+++ lua/kd/plugins/colorscheme.lua
@@ -1,18 +1,18 @@
 return {
-  -- {
-  --   "catppuccin/nvim",
-  --   name = "catppuccin",
-  --   priority = 1000,
-  --   config = function()
-  --     require("catppuccin").setup({
-  --       flavour = "mocha", -- latte, frappe, macchiato, mocha
-  --       dim_inactive = {
-  --         enabled = true, -- dims the background color of inactive window
-  --       },
-  --     })
-  --     vim.cmd.colorscheme("catppuccin")
-  --   end,
-  -- },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "latte", -- latte, frappe, macchiato, mocha
+        dim_inactive = {
+          enabled = true, -- dims the background color of inactive window
+        },
+      })
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  },
   {
     "rose-pine/neovim",
     name = "rose-pine",
