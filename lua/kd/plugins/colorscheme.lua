4befdcc2b6557387b0d6992a479bf507d6ba0628
Dheepak Krishnamurthy <1813121+kdheepak@users.noreply.github.com>
2025-08-12 01:39:38 -0400

    feat(colorscheme): switch to rose-pine as default colorscheme

--- lua/kd/plugins/colorscheme.lua
+++ lua/kd/plugins/colorscheme.lua
@@ -1,36 +1,36 @@
 return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      require("catppuccin").setup({
-        flavour = "mocha", -- latte, frappe, macchiato, mocha
-        dim_inactive = {
-          enabled = true, -- dims the background color of inactive window
-        },
-      })
-      vim.cmd.colorscheme("catppuccin")
-    end,
-  },
   -- {
-  --   "rose-pine/neovim",
-  --   name = "rose-pine",
+  --   "catppuccin/nvim",
+  --   name = "catppuccin",
+  --   priority = 1000,
   --   config = function()
-  --     require("rose-pine").setup({
-  --       styles = {
-  --         bold = true,
-  --         italic = false,
-  --       },
-  --       highlight_groups = {
-  --         Comment = { italic = true },
-  --         ["@markup.italic"] = { italic = true },
+  --     require("catppuccin").setup({
+  --       flavour = "mocha", -- latte, frappe, macchiato, mocha
+  --       dim_inactive = {
+  --         enabled = true, -- dims the background color of inactive window
   --       },
   --     })
-  --     -- load the colorscheme here
-  --     vim.cmd.colorscheme("rose-pine")
+  --     vim.cmd.colorscheme("catppuccin")
   --   end,
   -- },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    config = function()
+      require("rose-pine").setup({
+        styles = {
+          bold = true,
+          italic = false,
+        },
+        highlight_groups = {
+          Comment = { italic = true },
+          ["@markup.italic"] = { italic = true },
+        },
+      })
+      -- load the colorscheme here
+      vim.cmd.colorscheme("rose-pine")
+    end,
+  },
   -- {
   --   "projekt0n/github-nvim-theme",
   --   name = "github-theme",
