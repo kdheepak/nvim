7dcae246fdcbb21d72c9d6ae516fa2a5cdc65711
Dheepak Krishnamurthy <1813121+kdheepak@users.noreply.github.com>
2025-08-12 05:59:51 -0400

    feat(colorscheme): replace rose-pine with github-theme integration

--- lua/kd/plugins/colorscheme.lua
+++ lua/kd/plugins/colorscheme.lua
@@ -13,33 +13,33 @@ return {
   --     vim.cmd.colorscheme("catppuccin")
   --   end,
   -- },
-  {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    config = function()
-      require("rose-pine").setup({
-        styles = {
-          bold = true,
-          italic = false,
-        },
-        highlight_groups = {
-          Comment = { italic = true },
-          ["@markup.italic"] = { italic = true },
-        },
-      })
-      -- load the colorscheme here
-      vim.cmd.colorscheme("rose-pine")
-    end,
-  },
   -- {
-  --   "projekt0n/github-nvim-theme",
-  --   name = "github-theme",
+  --   "rose-pine/neovim",
+  --   name = "rose-pine",
   --   config = function()
-  --     require("github-theme").setup({
-  --       -- ...
+  --     require("rose-pine").setup({
+  --       styles = {
+  --         bold = true,
+  --         italic = false,
+  --       },
+  --       highlight_groups = {
+  --         Comment = { italic = true },
+  --         ["@markup.italic"] = { italic = true },
+  --       },
   --     })
-  --
-  --     vim.cmd("colorscheme github_light_colorblind")
+  --     -- load the colorscheme here
+  --     vim.cmd.colorscheme("rose-pine")
   --   end,
   -- },
+  {
+    "projekt0n/github-nvim-theme",
+    name = "github-theme",
+    config = function()
+      require("github-theme").setup({
+        -- ...
+      })
+
+      vim.cmd("colorscheme github_light_default") -- or github_dark_default
+    end,
+  },
 }
